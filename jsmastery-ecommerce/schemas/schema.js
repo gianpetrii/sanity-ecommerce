bb56bf32310b8884c1068f33760af137c18151e4
Gianluca <gianpetrii@gmail.com>
2022-09-13 17:46:26 -0300

    added assets and imported schemas to schema folder

--- jsmastery-ecommerce/schemas/schema.js
+++ jsmastery-ecommerce/schemas/schema.js
@@ -1,11 +1,19 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+
+
+
+// we import our schemas
+import product from './product';
+import banner from './banner';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([]),
+  types: schemaTypes.concat([ product, banner ]),
 })
+
+// then you just add everything in the localhost/desk view
