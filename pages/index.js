a688cf368a41bc3543f8d2d066400565b948fde2
Gianluca <gianpetrii@gmail.com>
2022-09-13 18:34:56 -0300

    basic index + global css

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div>index</div>
+    <>
+      HeadBanner
+
+      <div className='products-heading'>
+        <h2>Best Selling Products</h2>
+        <p>Speaker of many variations</p>
+      </div>
+
+      <div className='products-container'>
+        {['Product1', 'Product2'].map(
+          product => product
+        )}
+      </div>
+
+      Footer
+    </>
   )
 }
 
