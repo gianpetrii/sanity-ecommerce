c833039295aa3ea34c347db53008b9973afe3c32
Gianluca <gianpetrii@gmail.com>
2022-09-14 19:55:15 -0300

    navbar created with title and cart icon

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+// react
+import React from 'react';
+
+// next
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
+
+
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+
+      <p className='logo'>
+        <Link href="/">SANITY ECOMMERCE</Link>
+      </p>
+
+      <button type='button' className='cart-icon'>
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
   )
 }
 
