aa3a560ccff057fd631f2d67a07079d6f8808ed3
Gianluca <gianpetrii@gmail.com>
2022-09-14 01:22:45 -0300

    handle heroBanner prop to display info and link to product detail

--- components/HeroBanner/HeroBanner.jsx
+++ components/HeroBanner/HeroBanner.jsx
@@ -4,23 +4,30 @@ import React from 'react'
 // dependencies
 import Link from 'next/link';
 
-const HeroBanner = () => {
+// properties
+import { urlFor } from '../../lib/client';
+
+
+
+
+const HeroBanner = ({heroBanner}) => {
   return (
     <div className='hero-banner-container'>
 
       <div>
-        <p className='beats-solo'>SMALL TEXT</p>
+        <p className='beats-solo'>{heroBanner.smallText}</p>
         
-        <h3>MID TEXT</h3>
-        <img src="" alt="headphones" className='hero-banner-image'/>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="headphones" className='hero-banner-image'/>
 
         <div>
-          <Link href="/product/ID">
-            <button type='button'>BUTTON TEXT</button>
+          <Link href={"/product/" + heroBanner.product }>
+            <button type='button'>{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
             <h5>Description</h5>
-            <p>ACTUAL DESCRIPTION</p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
