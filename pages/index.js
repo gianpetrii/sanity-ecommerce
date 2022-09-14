77fc81ebb18c289baa69b6ac4f2686be6cb39e83
Gianluca <gianpetrii@gmail.com>
2022-09-14 01:22:13 -0300

    fetch of products and banners and pass it as props

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,24 @@ import HeroBanner from '../components/HeroBanner/HeroBanner.jsx';
 import Product from '../components/Product/Product.jsx';
 import FooterBanner from '../components/FooterBanner/FooterBanner.jsx';
 
+//sanity
+import { client } from '../lib/client.js';
 
 
-const index = () => {
+
+const Home = ({products, banners}) => {
+
+  const array = ["2", "1"];
   return (
     <>
-      <HeroBanner />
+
+      <HeroBanner heroBanner={ banners.length && banners[0]} />
+      {/* quite interestingly I can directly pass object or array with 
+      object inside when I first ask about lenght and on the HeroBanner 
+      component I get the prop {heroBanner}. Else I have to use 
+      {...banners[0]} which in turn opens it and inside HeroBanner component
+      props I must directly call {smallText, and such}
+      */}
 
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
@@ -21,14 +33,33 @@ const index = () => {
       </div>
 
       <div className='products-container'>
-        {['Product1', 'Product2'].map(
-          product => product
+        {products?.map(
+          product => product.name
         )}
       </div>
 
       <FooterBanner />
     </>
   )
-}
+};
 
-export default index;
+
+
+
+// whenever fetching data from API or server
+export const getServerSideProps = async () => {
+  const productsQuery = '*[_type == "product"]' // *=all which types are products
+  const products = await client.fetch(productsQuery);
+
+  const bannerQuery = '*[_type == "banner"]' // *=all which types are products
+  const banners = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, banners }
+  }
+};
+
+
+
+
+export default Home;
